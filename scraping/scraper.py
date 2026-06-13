d419ea926ded2661bd82943ad39fdeec0a25cd84
Carolina Cardoso (Jay) <carolinaliboriocardoso@gmail.com>
2026-06-13 11:44:15 +0100

    fixed some scraping logic issues

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -23,10 +23,12 @@ def scrape_data():
         response1 = requests.get(URLS["apambiente"], headers=headers)
         soup1 = BeautifulSoup(response1.text, 'html.parser')
         selector1 = ".right-header-column .text-container:nth-of-type(n+2)"
-        relevant_containers = soup1.select(selector1) 
+        selector2 = ".right-header-column .additional-info .text-container" 
+        info_containers = soup1.select(selector1) 
+        dateTime_containers = soup1.select(selector2) 
         
         apambiente_metrics = {}
-        for container in relevant_containers:
+        for container in info_containers:
             title_element = container.select_one(".title")
             value_element = container.select_one(".value")
             if title_element and value_element:
@@ -34,6 +36,27 @@ def scrape_data():
                 numeric_match = re.findall(r"[-+]?\d*\.\d+|\d+", value_element.text) #only numbers no text and stuff
                 if numeric_match:
                     apambiente_metrics[title] = numeric_match[0]
+                    
+        for container in dateTime_containers:
+            date_element = container.select_one(".title")
+            time_element = container.select_one(".text")
+            
+            if date_element and time_element:
+                title1 = "Data"
+                title2 = "Hora"
+                
+                date_text = date_element.text.strip()
+                time_text = time_element.text.strip()
+                
+                date_match = re.search(r"\d+\s+[a-zA-Zçíúâêôãõáéó]+\s+\d+", date_text) 
+                time_match = re.search(r"\d{2}:\d{2}", time_text)
+                
+                if date_match:
+                    apambiente_metrics[title1] = date_match.group()
+                    
+                if time_match:
+                    apambiente_metrics[title2] = time_match.group()
+                    
         
         if apambiente_metrics:
             scraped_data["apambiente"].append(apambiente_metrics)
@@ -48,11 +71,20 @@ def scrape_data():
         
         table_rows = soup2.select('table.tablelist tr[class^="bkg_"]')
         
+        seen_hours = set()
+        
         for row in table_rows:
             cells = row.find_all('td')
-            if len(cells) >= 9:
+            if len(cells) >= 10:  
+                hora = cells[0].text.strip()
+                
+                if hora == "00h" and "00h" in seen_hours:
+                    break  
+                
+                seen_hours.add(hora)
+                
                 scraped_data["ipma"].append({
-                    "Hora": cells[0].text.strip(),
+                    "Hora": hora,
                     "Ondulacao": cells[2].text.strip(),
                     "Temperatura": cells[9].text.strip()  
                 })
